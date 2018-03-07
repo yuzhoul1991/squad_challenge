25fe30e32b300b80c31cf5f51ba08afb2642da36
Yuzhou Liu <yuzhoul.eecs@gmail.com>
2018-03-07 08:05:17 -0800

    remove some junk

--- code/ptr_net.py
+++ code/ptr_net.py
@@ -9,25 +9,6 @@ class PointerNet(object):
     def __init__(self, hidden_size):
         self.hidden_size = hidden_size
 
-    # def get_attention_ptrntwk(attention_states, init_state, passage_lengths, indices, scope=None):
-    #   with variable_scope.variable_scope(scope):
-    #     attention_states = tf.transpose(attention_states, [1, 0, 2])
-    #     attn_size = attention_states.get_shape()[2].value
-    #     print("Ptr Network attention size = %d" %attn_size)
-    #     labels = tf.unstack(indices, axis=1)
-    #     attention_mechanism = BahdanauAttention(
-    #                                     attn_size,
-    #                                     attention_states,
-    #                                     memory_sequence_length=passage_lengths)
-    #     answer_ptr_input_fn = lambda curr_input, atts : tf.concat([atts, init_state], -1) # independent of inputs
-    #     start_cell = AttentionWrapper(
-    #                             tf.nn.rnn_cell.BasicLSTMCell(attn_size),
-    #                             attention_mechanism,
-    #                             attention_layer_size = attn_size,
-    #                             cell_input_fn = answer_ptr_input_fn)
-    #     logits, _ = tf.nn.static_rnn(start_cell, labels, dtype = tf.float32)
-    #     return logits
-
     def build_graph(self, source_hidden_states, context_mask, labels, question_hiddens, question_mask):
         def get_query_pooled(query_states, query_lengths, scope="pool_query"):
             with vs.variable_scope(scope):
